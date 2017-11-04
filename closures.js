49a78ec1cf82985bb4ae52cddcdccea09184dd3f
Paulo <pauloilarde@gmail.com>
2017-11-04 16:52:32 -0200

    Closures

--- closures.js
+++ closures.js
@@ -4,3 +4,34 @@
 // fechamento, encerramento, encarceramento
 
 // vamos voltar a um exemplo já utilizado
+
+var helloWorld = function () {
+   return 'Hello World!';
+}
+
+helloWorld; // [Function: helloWorld]
+
+helloWorld(); // 'Hello World!'
+
+// até aqui ok
+
+// agora vamos modificar o exemplo
+// inserindo uma variável dentro da função
+
+var helloWorld = function () {
+   var message = 'Hello world';
+   return function () {
+      return message;
+   };
+};
+
+helloWorld; //[Function: helloWorld]
+> helloWorld(); // [Function]
+> helloWorld()(); //'Hello world'
+
+// quando a gente colocou uma função dentro da outra
+// em teoria, a função interna se desconectou
+// por isso, não deveria ser possível conseguirmos enxergar
+// a variável message impressa
+// pela lógica, a variável message seria undefined
+// nós conseguimos enxergar por causa do recurso de closure
